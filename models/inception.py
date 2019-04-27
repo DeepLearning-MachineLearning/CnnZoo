4b4469a360c90d33d3a5f171974a579c575d23ee
Tao Li <litaotju@live.cn>
2019-04-27 15:56:28 +0800

    add traning helper softmax head

--- models/inception.py
+++ models/inception.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import (
         Concatenate, AveragePooling2D,
         Dropout, Softmax, Dense)
 
-def GoogLeNet(input_shape=(224,224,3)):
+def GoogLeNet(input_shape=(224,224,3), is_training=False):
     def inception_block(num_1x1, num_3x3_reduc, num_3x3, num_5x5_reduc, num_5x5, num_pool_proj, x):
         b1 = Conv2D(num_1x1,1,strides=1,padding="same",activation="relu")(x)
         b2 = Conv2D(num_3x3_reduc,1,strides=1,padding="same", activation="relu")(x)
@@ -17,6 +17,18 @@ def GoogLeNet(input_shape=(224,224,3)):
         x = Concatenate(axis=-1)([b1,b2,b3,b4])
         return x
 
+    def softmax_head(x):
+        '''Softmax head to help the traning
+        '''
+        y = AveragePooling2D(pool_size=(5,5),strides=3)(x)
+        y = Conv2D(1024, 1, strides=1, padding="same", activation="relu")(y)
+        y = Flatten()(y)
+        y = Dense(1000)(y)
+        y = Dense(1000)(y)
+        y = Softmax()(y)
+        return y
+
+    outputs = []
     inputs = Input(shape=input_shape)
     x = Conv2D(64,7, strides=2, padding="same",activation="relu")(inputs)
     x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="same")(x)
@@ -26,9 +38,11 @@ def GoogLeNet(input_shape=(224,224,3)):
     x = inception_block(128,128,192,32,96,64,x)
     x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="same")(x)
     x = inception_block(192,96,208,16,48,64,x)
+    if is_training: outputs.append(softmax_head(x))
     x = inception_block(160,112,224,24,64,64,x)
     x = inception_block(128,128,256,24,64,64,x)
     x = inception_block(112,144,288,32,64,64,x)
+    if is_training: outputs.append(softmax_head(x))
     x = inception_block(256,160,288,32,64,64,x)
     x = MaxPooling2D(pool_size=(2,2),strides=(2,2),padding="same")(x)
     x = inception_block(256,160,288,32,64,64,x)
@@ -38,10 +52,12 @@ def GoogLeNet(input_shape=(224,224,3)):
     x = Flatten()(x)
     x = Dense(1000)(x)
     x = Softmax()(x)
-    return  Model(inputs=inputs,outputs=x)
+    outputs.append(x)
+    return  Model(inputs=inputs,outputs=outputs)
 
 def google_net(input_shape=(224,224,3)):
     return  GoogLeNet(input_shape)
 
 if __name__ == "__main__":
-    GoogLeNet((448,448,3)).summary()
+    GoogLeNet((448,448,3), is_training=True).summary()
+    GoogLeNet((448,448,3), is_training=False).summary()
