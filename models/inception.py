505395ef2e42c25a8156ea17c571b33e77a37745
Tao Li <tali@nvidia.com>
2019-05-18 16:32:15 +0800

    complete the inception_resnet_v2

--- models/inception.py
+++ models/inception.py
@@ -2,9 +2,9 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (
         Conv2D, MaxPooling2D, Flatten, Input, 
         Concatenate, AveragePooling2D,
-        Dropout, Softmax, Dense)
+        Dropout, Softmax, Dense, Add, Lambda)
 
-def GoogLeNet(input_shape=(224,224,3), is_training=False):
+def GoogLeNet(input_shape=(224,224,3), class_num=1000, is_training=False):
     def inception_block(num_1x1, num_3x3_reduc, num_3x3, num_5x5_reduc, num_5x5, num_pool_proj, x):
         b1 = Conv2D(num_1x1,1,strides=1,padding="same",activation="relu")(x)
         b2 = Conv2D(num_3x3_reduc,1,strides=1,padding="same", activation="relu")(x)
@@ -50,93 +50,130 @@ def GoogLeNet(input_shape=(224,224,3), is_training=False):
     x = AveragePooling2D(pool_size=(7,7),strides=1)(x)
     x = Dropout(0.4)(x)
     x = Flatten()(x)
-    x = Dense(1000)(x)
+    x = Dense(class_num)(x)
     x = Softmax()(x)
     outputs.append(x)
     return  Model(inputs=inputs,outputs=outputs)
 
-def inception_v2(input_shape=(224,224,3)):
-    def inception_block_a(num_1x1, num_3x3_reduc, num_3x3, num_5x5_reduc, num_5x5, num_pool_proj, x):
-        '''Inception block from Fig 5 of https://arxiv.org/pdf/1512.00567v3.pdf
-           Compared with GoogLeNet inception block, only the 5x5 convolution 
-           is replaced by two continious 3x3 convolution layers
-        '''
-
-        b1 = Conv2D(num_1x1,1,strides=1,padding="same",activation="relu")(x)
-        b2 = Conv2D(num_3x3_reduc,1,strides=1,padding="same", activation="relu")(x)
-        b2 = Conv2D(num_3x3, 3,strides=1,padding="same", activation="relu")(b2)
-        b3 = Conv2D(num_5x5_reduc,1,strides=1,padding="same", activation="relu")(x)
-        b3 = Conv2D(num_5x5,3,strides=1,padding="same", activation="relu")(b3)
-        b3 = Conv2D(num_5x5,3,strides=1,padding="same", activation="relu")(b3)
-        b4 = MaxPooling2D(pool_size=(3,3),strides=(1,1),padding="same")(x)
-        b4 = Conv2D(num_pool_proj,1,strides=1,padding="same", activation="relu")(b4)
-        #suppose the input is HWC format
-        x = Concatenate(axis=-1)([b1,b2,b3,b4])
+def inception_resnet_v2(input_shape=(224,224,3), class_num=1000):
+    '''Google's Inception-ResNet v2 model
+    '''
+    def inception_block(inputs, branches, activation="relu"):
+        branch_tensors = []
+        for i, branch in enumerate(branches):
+            branch_tensors.append([])
+            x = inputs
+            for layer in branch:
+                ltype, args = layer[0], layer[1:]
+                if ltype == "Conv2D":
+                    x = Conv2D(*args, activation=activation)(x)
+                elif ltype == "MaxPooling2D":
+                    x = MaxPooling2D(*args)(x)
+                else:
+                    raise ValueError ("Invalid layer type" + ltype)
+                branch_tensors[i].append(x)
+        CONCAT_DIM = -1
+        #concat the last tensor of each branch in C channel
+        x = Concatenate(CONCAT_DIM)([branch[-1] for branch in branch_tensors])
         return x
 
-    def inception_block_b(k_b1, k_pool, k_b2_reduc, k_b2, k_b3_reduc, k_b3, n):
-        '''Inception block from Fig 6 of https://arxiv.org/pdf/1512.00567v3.pdf
-           Compared with GoogLeNet inception block, the nxn convolution is replaced 
-           by two continuous 1xn and nx1 conolution layers
-        '''
+    def inception_v4_sa(x):
+        branches = [ [("MaxPooling2D",3,2,"valid")],
+                     [("Conv2D",96,3,2,"valid")]]
+        return inception_block(x, branches)
 
-        b1 = Conv2D(k_b1,1,strides=1,padding="same",activation="relu")(x)
+    def inception_v4_sb(x):
+        branches =  [ [("Conv2D",64,1,1, "same"), ("Conv2D",96,3,1,"valid")],
+                      [("Conv2D",64,1,1, "same"), ("Conv2D",64,(7,1),1,"same"), ("Conv2D", 64, (1,7), 1, "same"), ("Conv2D",96,3,1, "valid")]]
+        return inception_block(x, branches)
 
-        b2 = Conv2D(k_b2_reduc, 1, strides=1,padding="same", activation="relu")(x)
-        b2 = Conv2D(k_b2, (1,n), strides=1,padding="same", activation="relu")(b2)
-        b2 = Conv2D(k_b2, (n,1), strides=1,padding="same", activation="relu")(b2)
+    def inception_v4_sc(x):
+        branches = [ [("Conv2D",192,3,2, "valid"),],
+                     [("MaxPooling2D", 3,2,"valid")]]
+        return inception_block(x, branches)
 
-        b3 = Conv2D(k_b3_reduc, 1, strides=1,padding="same", activation="relu")(x)
-        b3 = Conv2D(k_b3, (1,n), strides=1,padding="same", activation="relu")(b3)
-        b3 = Conv2D(k_b3, (n,1), strides=1,padding="same", activation="relu")(b3)
-        b3 = Conv2D(k_b3, (1,n), strides=1,padding="same", activation="relu")(b3)
-        b3 = Conv2D(k_b3, (n,1), strides=1,padding="same", activation="relu")(b3)
-
-        b4 = MaxPooling2D(pool_size=(3,3),strides=(1,1),padding="same")(x)
-        b4 = Conv2D(num_pool_proj,1,strides=1,padding="same", activation="relu")(b4)
-        #suppose the input is HWC format
-        x = Concatenate(axis=-1)([b1,b2,b3,b4])
+    def inception_resnet_v2_a(x):
+        branches = [ [("Conv2D",31,1,1,"same")],
+                     [("Conv2D",32,1,1,'same'), ('Conv2D', 32,3,1,'same')],
+                     [('Conv2D',32,1,1,'same'), ('Conv2D', 48,3,1,'same'),('Conv2D', 64,3,1,'same')]]
+        x = inception_block(x, branches)
+        x = Conv2D(384,1,1,'same')(x)
+        return x
+    
+    def inception_resnet_v2_b(x):
+        #TODO: confirm 128->160->192 is right for 1x7->7x1 branch?
+        branches = [[('Conv2D',192,1,1,'same')],
+                    [('Conv2D',128,1,1,'same'),('Conv2D',160,(1,7),1,'same'),('Conv2D',192,(7,1),1,'same')]]
+        x = inception_block(x, branches)
+        #TODO: Is this correct?
+        x = Conv2D(1152,1,1,'same')(x)
         return x
 
-    def inception_block_c(k_b1, k_pool, k_b2_reduc, k_b2, k_b3_reduc, k_b3, n):
-        '''Inception block from Fig 7 of https://arxiv.org/pdf/1512.00567v3.pdf
-           Compared with GoogLeNet inception block, part of the nxn convolution is replaced 
-           by two continuous 1xn and nx1 conolution layers. And the inception block is made to be wider than deeper
-        '''
-
-        b1 = Conv2D(k_b1,1,strides=1,padding="same",activation="relu")(x)
-
-        b2 = Conv2D(k_b2_reduc, 1, strides=1,padding="same", activation="relu")(x)
-        b2_1 = Conv2D(k_b2, (1,n), strides=1,padding="same", activation="relu")(b2)
-        b2_2 = Conv2D(k_b2, (n,1), strides=1,padding="same", activation="relu")(b2)
-
-        b3 = Conv2D(k_b3_reduc, 1, strides=1,padding="same", activation="relu")(x)
-        b3 = Conv2D(k_b3, (3,3), strides=1,padding="same", activation="relu")(b3)
-        b3_1 = Conv2D(k_b3, (1,n), strides=1,padding="same", activation="relu")(b3)
-        b3_2 = Conv2D(k_b3, (n,1), strides=1,padding="same", activation="relu")(b3)
-
-        b4 = MaxPooling2D(pool_size=(3,3),strides=(1,1),padding="same")(x)
-        b4 = Conv2D(num_pool_proj,1,strides=1,padding="same", activation="relu")(b4)
-        #suppose the input is HWC format
-        x = Concatenate(axis=-1)([b1,b2_1, b2_2, b3_1, b3_3, b4])
+    def inception_resnet_v2_c(x):
+        branches = [ [("Conv2D",192,1,1,"same")],
+                     [("Conv2D",192,1,1,'same'), ('Conv2D', 224, (1,3),1,'same'), ('Conv2D',256,(3,1),1,'same')]]
+        x = inception_block(x, branches)
+        x = Conv2D(2048,1,1,'same')(x)
         return x
+
+    def inception_v4_ra(x):
+        branches = [ [('MaxPooling2D',3,2,'valid')],
+                     [('Conv2D',384,3,2,'valid')],
+                     [('Conv2D',256,1,1,'same'),('Conv2D',256,3,1,'same'),('Conv2D',384,3,2,'valid')]]
+        return inception_block(x, branches)
+
+    def inception_resnet_v2_rb(x):
+        branches = [[('MaxPooling2D', 3,2,'valid')],
+                    [('Conv2D',256,1,1,'same'),('Conv2D',384,3,2,'valid')],
+                    [('Conv2D',256,1,1,'same'),('Conv2D',256,3,2,'valid')],
+                    [('Conv2D',256,1,1,'same'),('Conv2D',256,3,1,'same'),('Conv2D',256,3,2,'valid')]]
+        return inception_block(x, branches)
+
+    def residual_block(x, flow, scale=1.0, activation=None):
+        outputs = flow(x)
+        #TODO: support NCHW
+        #asumming it's NHWC
+        strides = ((x.shape[-3]-1)//outputs.shape[-3]+1,
+                   (x.shape[-2]-1)//outputs.shape[-2]+1)
+        if x.shape != outputs.shape or strides != (1,1):
+            # Use Conv2D to make the shape concistent, no activation
+            x = Conv2D(outputs.shape.as_list()[-1], 1, strides, "same")(x)
+
+        def Scale(i):
+            return i*scale
+        x = Add()([x, Lambda(Scale)(outputs)])
+        if activation:
+            x = activation(x)
+        return x
+
     outputs = []
     inputs = Input(shape=input_shape)
     x = Conv2D(32,3, strides=2, padding="valid",activation="relu")(inputs)
     x = Conv2D(32,3, strides=1, padding="valid",activation="relu")(x)
     x = Conv2D(64,3, strides=1, padding="same",activation="relu")(x)
-    x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="valid")(x)
 
-    x = Conv2D(80,3, strides=1, padding="valid",activation="relu")(x)
-    x = Conv2D(192,3, strides=2, padding="valid",activation="relu")(x)
-    x = Conv2D(288,3, strides=1, padding="same",activation="relu")(x)
+    residual_scale = 0.2
+    x = inception_v4_sa(x)
+    x = inception_v4_sb(x)
+    x = inception_v4_sc(x)
+    for _ in range(5):
+        x = residual_block(x, inception_resnet_v2_a, scale=residual_scale)
+    x = inception_v4_ra(x)
+    for _ in range(10):
+        x = residual_block(x, inception_resnet_v2_b, scale=residual_scale)
+    x = inception_resnet_v2_rb(x)
+    for _ in range(5):
+        x = residual_block(x, inception_resnet_v2_c, scale=residual_scale)
+    x = AveragePooling2D()(x)
+    x = Dropout(0.8)(x)
+    
+    # This head can be changed
+    x = Flatten()(x)
+    x = Dense(class_num)(x)
+    x = Softmax()(x)
 
-    x = inception_block_a(64,96,128,16,32,32,x)
-    x = inception_block_a(64,96,128,16,32,32,x)
-    x = inception_block_a(64,96,128,16,32,32,x)
     outputs.append(x)
     return  Model(inputs=inputs,outputs=outputs)
-
 
 
 def google_net(input_shape=(224,224,3)):
@@ -145,4 +182,4 @@ def google_net(input_shape=(224,224,3)):
 if __name__ == "__main__":
     GoogLeNet((448,448,3), is_training=True).summary()
     GoogLeNet((448,448,3), is_training=False).summary()
-    inception_v2((299,299,3)).summary()
+    inception_resnet_v2((299,299,3)).summary()
